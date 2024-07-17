cae2bd2a869305726672f6593ec44cf206e20a50
Tomás Serra <tomi.serra@gmail.com>
2024-07-17 13:05:20 -0300

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -51,7 +51,7 @@ registerRoute(
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
   ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
-  new StaleWhileRevalidate({
+  new CacheFirst({
     cacheName: 'images',
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
